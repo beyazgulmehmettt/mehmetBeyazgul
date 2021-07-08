d638521999773e8a1faa0e6a2a818faf77993b60
NetAcad DEVASC <changeme@example.com>
2021-07-08 22:10:00 +0000

    dosyalar eklendi

---
+++ mehmet.py
@@ -0,0 +1,6 @@
+# Fill in this file with the code from parsing JSON exercise
+import json
+dosya=open("myfile.json","r")
+json_dosya=json.load(dosya)
+print("Ad    : ",json_dosya["kimlik"]["ad"])
+print("Soyad : ",json_dosya["kimlik"]["soyad"])
